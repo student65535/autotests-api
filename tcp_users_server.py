01c4cb16ec3bbed097d7195fa75d70a5b4e2883a
student65535 <nadezhda.gu6eva@gmail.com>
2026-01-11 11:19:59 +0700

    5.7 Знакомство с TCP/IP протоколами. Практика работы с TCP/IP протоколом

---
+++ tcp_users_server.py
@@ -0,0 +1,34 @@
+import socket  # Импортируем модуль socket для работы с сетевыми соединениями
+
+messages = []
+
+def server():
+    # Создаем TCP-сокет
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Привязываем его к адресу и порту
+    server_address = ('localhost', 12345)
+    server_socket.bind(server_address)
+
+    # Начинаем слушать входящие подключения (максимум 10 в очереди)
+    server_socket.listen(10)
+
+    while True:
+        # Принимаем соединение от клиента
+        client_socket, client_address = server_socket.accept()
+        print(f"Пользователь с адресом {client_address} подключился к серверу")
+
+        # Получаем данные от клиента
+        data = client_socket.recv(1024).decode()
+        print(f"Пользователь с адресом {client_address} отправил сообщение: {data}")
+        messages.append(data)
+
+        # Отправляем ответ клиенту
+        client_socket.send('\n'.join(messages).encode())
+
+        # Закрываем соединение с клиентом
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    server()
