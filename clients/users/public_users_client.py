d8348920757f3cfe5304d293a757a30373e2abfc
student65535 <nadezhda.gu6eva@gmail.com>
2026-01-26 07:16:26 +0700

    lesson. add builder

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -3,9 +3,22 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
 
 
-class UserRequestDict(TypedDict):
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
     """
@@ -16,12 +29,20 @@ class UserRequestDict(TypedDict):
     middleName: str
 
 
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
+
 class PublicUsersClient(APIClient):
     """
     Клиент для работы с /api/v1/users
     """
 
-    def create_user_api(self, request: UserRequestDict) -> Response:
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
         Метод выполняет создание пользователя.
 
@@ -29,3 +50,18 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
+
+
+# Добавляем builder для PublicUsersClient
+def get_public_users_client() -> PublicUsersClient:
+    """
+    Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию PublicUsersClient.
+    """
+    return PublicUsersClient(client=get_public_http_client())
